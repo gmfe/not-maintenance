3ebf7d95eab4abb425db5a0123685a2054f6bedb
liyatang <liyatang@qq.com>
2020-02-18 19:22:09 +0800

    gmrnbt

---
+++ gmrnbt/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'plugins': [
+    'gm'
+  ],
+  'extends': ['plugin:gm/recommended'],
+
+  'rules': {
+    'react/display-name': 0
+  },
+  'globals': {
+    '__DEV__': true
+  }
+}
